af34276de10789c751061527cb424d717c100a0d
kirikover <52546826+kirikover@users.noreply.github.com>
2020-02-13 00:39:32 +0300

    goodbye

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,6 @@ public class Main {
             array[i] = array[i] * 5;
             System.out.print(array[i] + " ");
         }
-        System.out.print("Hello!");
+        System.out.print("goodbye!");
 }
 }
