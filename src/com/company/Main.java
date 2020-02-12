464c98b5467963a3e358bee92f6f7e56d43eb9d8
kirikover <52546826+kirikover@users.noreply.github.com>
2020-02-13 00:46:17 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,6 @@ public class Main {
             array[i] = array[i] * 5;
             System.out.print(array[i] + " ");
         }
-        System.out.print("goodbye!");
+        System.out.print("goodbye and see you later!");
 }
 }
