f383b6ad1e7ec6f4ed86d81120b2b3730ae40e8b
kirikover <52546826+kirikover@users.noreply.github.com>
2020-02-13 00:09:36 +0300

    second version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.round((Math.random() * 20) - 10);
+            System.out.print(array[i] + " ");
+        }
     }
 }
